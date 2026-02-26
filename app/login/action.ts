41f5f631ff709b123eb9cbbf6c09c67cc00b6eef
Elias <elias.ordaz25@gmail.com>
2026-02-25 18:16:04 -0600

    refactor signUpNewUser function to improve logging and type handling

--- app/login/action.ts
+++ app/login/action.ts
@@ -3,15 +3,16 @@ import { createClient } from '@/lib/supabase/server'
 import { NextResponse} from 'next/server'
 
 export async function signUpNewUser(formData: FormData) {
+  console.log('formData', formData.get('email'), formData.get('password'))
     const supabase = await createClient()
     const { data, error } = await supabase.auth.signUp({
-    email: String(formData.get('email')),
-    password: String(formData.get('password')),
+    email: formData.get('email') as string,
+    password: formData.get('password') as string,
     options: {
       emailRedirectTo: 'http://localhost:3000',
     },
   })
-
+  console.log('error', error)
   console.log('data', data)
 
 }
